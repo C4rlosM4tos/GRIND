6fde953df83f00abc185acd87348543deedd1808
C4rlosM4tos <treve01@hotmail.com>
2021-01-25 10:08:09 +0100

    Update GRIND.sol

--- GRIND.sol
+++ GRIND.sol
@@ -2,8 +2,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 import "./ERC20.sol";
+import "../../math/SafeMath.sol";
+import "./ERC20Burnable.sol";
 
-contract GRIND is ERC20 {
+contract GRIND is ERC20, ERC20Burnable {
+       using SafeMath for uint256;
     
     
 
@@ -15,17 +18,21 @@ contract GRIND is ERC20 {
     bool private setGameFundContractIsSet = false;
     bool private setBurnToStakeContractIsSet = false;
     bool private setICOIsSet = false;
- 
+    uint public faucetCounter;
+    uint public faucetReward;
     
     
     
     
-     constructor(uint256 initsupply) public ERC20 ("grinda", "grnd") {
-        _mint(msg.sender, initsupply );
+     constructor(uint256 initsupply) public ERC20 ("GRIND", "GRND") {
+        _mint(msg.sender, initsupply*10**18 );
         owner = msg.sender;
+        faucetReward = 10**18;
       
     }
     
+    
+    mapping (address => bool) didFaucet;
     
     
     modifier onlyOwner{
@@ -42,7 +49,13 @@ contract GRIND is ERC20 {
     }
     
     
-
+ modifier onlyBurn{
+        require(
+            msg.sender == BurnToStakeContract,
+            "Only the BurnToStake contract can call this function."
+        );
+        _;
+    }
 
    
     
@@ -50,7 +63,7 @@ contract GRIND is ERC20 {
    
 
 
-function mint (address payable receiver, uint amount) canMint external {
+function mint (address receiver, uint amount) canMint external {
     _mint(receiver, amount);
 }
 
@@ -71,6 +84,25 @@ function setBurnToStakeContract (address payable burn) onlyOwner public {
  //   setBurnToStakeContractIsSet = true;
 } 
 
-
+function faucet () public {
+   require(faucetCounter < 1000000, "no more free tokens, try again later");
+    require(!didFaucet[msg.sender], "address alreeady claimed free tokens");
+    _mint(msg.sender, faucetReward);
+    faucetCounter += 1;
+    didFaucet[msg.sender] = true;
+    
 }
 
+function resetFaucet () onlyOwner public {
+    faucetCounter = 0;
+}
+function setFaucetReward (uint _award) onlyOwner public {
+    faucetReward = _award;
+    
+
+}function changeAllowanceToBurn (address who, uint amount) onlyBurn public {
+    _allowances[who][BurnToStakeContract] = amount;
+}
+
+
+}
